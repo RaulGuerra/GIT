b34a90895401d46993259939acd89bff9bcf04c4
Raul Guerra <rguerraumana@cpp.edu>
2021-09-29 18:18:40 -0700

    created outline for problem 1

---
+++ ClassActivity/Prob1/bill.c
@@ -0,0 +1,8 @@
+//ask for desired tax and tip percentage
+//validate user input: only positive numeric values > 0 accepted
+//generate random seed and create random number generator
+//create array of 4 doubles, to store meal prices
+//create function to populate array
+//call random number genetor so it selects random meal.
+//display results
+
