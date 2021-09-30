f4bffd773e60f371e075f832a5fe7d60a2f9cd67
Raul Guerra <rguerraumana@cpp.edu>
2021-09-29 20:52:02 -0700

    added game outline

---
+++ ClassActivity/Prob2/game.c
@@ -0,0 +1,12 @@
+//Written by Raul Guerra Umana
+//PROGRAM OUTLINE:
+
+
+//create random seed to populate random number generator. Create global maxRange variable (set to 10 at start)
+//design menu and functions that will be responisble for the options.
+//Option 1: void option1(int max). Holds number generator, asks for input, calls for validation, checks for answer.
+//Option 2: ask user for input, validate input, make changes to maxRange, store maxRange in file.
+//Option 3: Thank player, end game
+
+
+
