cc526af80ce4141ac9e9ed78b8e8cb3373bb5e53
Raul Guerra <rguerraumana@cpp.edu>
2021-12-08 18:00:22 -0800

    Turned off canonical mode

--- Final_Project/kilo.c
+++ Final_Project/kilo.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 
 struct termios orig_termios;
+
 void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
@@ -10,14 +11,19 @@ void disableRawMode() {
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
+
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
+
   enableRawMode();
   char c;
+
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
   return 0;
 }
